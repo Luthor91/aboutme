5dee8028f5a6b8ee23a07debfbcce8fc9994584e
Killian Lopez <killian.lopez@supdevinci-edu.fr>
2024-10-10 17:10:42 +0200

    update news data

--- public/projects/scripts.js
+++ public/projects/scripts.js
@@ -10,7 +10,7 @@ const projects = [
     {
         name: "SelfActu",
         description: "Un petit site web statique permettant de se mettre à jour sur les dernières actualités.",
-        technologies: ["HTML", "CSS", "Javascript", "Makefile"],
+        technologies: ["HTML", "CSS", "Javascript", "Python", "Makefile"],
         link: "https://luthor91.github.io/aboutme/public/news/"
     },
     {
